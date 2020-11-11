fb644be4ca182fdc446a52b483033a437b0933ff
拓也 落合 <takuya-ochiai@aeroasahi.co.jp>
2020-11-11 19:10:40 +0900

    firebaseへのアップデートのため含めた

---
+++ dist/precache-manifest.490050b7f86683e42f357de3d35b3a2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053da15ad0a0c4c9d509",
+    "url": "/css/app.19010b46.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "66c569e6e8cb85dd8a5944bd73fe39b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5ea3d769e6cacab730",
+    "url": "/js/about.fe960957.js"
+  },
+  {
+    "revision": "053da15ad0a0c4c9d509",
+    "url": "/js/app.0c21377b.js"
+  },
+  {
+    "revision": "d1f80ae2a35c46b4d596",
+    "url": "/js/chunk-vendors.60769280.js"
+  },
+  {
+    "revision": "93f02292ac0a17d87ca5a8728eed04f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
